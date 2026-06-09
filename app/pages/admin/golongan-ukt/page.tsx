ad52e39a0a5ee219935b7337804722faaed6bbfe
revivalxvr <revivaliskandar@gmail.com>
2026-06-09 14:01:34 +0800

    - CRUD golongan UKT -perbaikan AdminSide Bar

--- app/pages/admin/golongan-ukt/page.tsx
+++ app/pages/admin/golongan-ukt/page.tsx
@@ -1,8 +1,136 @@
-
-import React from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 import MyBarChart from '../../../components/myBarChart';
+import api from '@/app/lib/axiosInstance';
+
+interface GolonganUKT {
+  id: string;
+  group: string;
+  amount: number;
+  createdAt: string;
+}
+
+const getGolUkt = async () => {
+  const res = await api.get('/tf-groups');
+  return res.data.data;
+}
+
+const addGolUkt = async (data: { group: string; amount: number }) => {
+  const res = await api.post('/tf-groups', data);
+  return res.data;
+}
+
+const updateGoUkt = async (id: string, data: { group?: string; amount?: number }) => {
+  const res = await api.put(`/tf-groups/${id}`, data);
+  return res.data;
+}
+
+const deleteGolUkt = async (id: string) => {
+  const res = await api.delete(`/tf-groups/${id}`);
+  return res.data;
+}
 
 const GolUKTPage = () => {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [selectedGolUkt, setSelectedGolUkt] = useState<Partial<GolonganUKT>>({});
+  const [newGolUkt, setNewGolUkt] = useState({
+    group: "",
+    amount: null,
+  })
+  const [golUktList, setGolUktList] = useState<GolonganUKT[]>([]);
+
+   const fetchData = async () => {
+      try {
+        const data = await getGolUkt();
+        setGolUktList(data);
+      } catch (error) {
+        console.log("Gagal mengambil data Golongan UKT ==",error);
+      }
+    }
+
+  //ambil data awal
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+ const openEditModal = (ukt: GolonganUKT) => {
+  setSelectedGolUkt(ukt);
+  setIsEditModalOpen(true);
+ } 
+
+ const closeEditModal = () => {
+   setIsEditModalOpen(false);
+   setSelectedGolUkt({});
+ }
+
+ const handleInputChange = (e:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setSelectedGolUkt((prev) => ({ ...prev, [name]: name === "amount" ? Number(value) : value }));
+ }
+ const handleNewGolUktChange = (e:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setNewGolUkt((prev) => ({ ...prev, [name]: name === "amount" ? Number(value) : value }));
+ }
+
+const handleSave = async (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  if (!selectedGolUkt.id) return;
+  try {
+    const updated = await updateGoUkt(selectedGolUkt.id, {
+      group: selectedGolUkt.group,
+      amount: selectedGolUkt.amount,
+    });
+    fetchData();
+    closeEditModal();
+  } catch (error) {
+    console.log("Gagal menyimpan perubahan Golongan UKT ==",error);
+  }
+}
+
+const handleAddNewGolUkt = async (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault();
+  try {
+    const playload = {
+      group: newGolUkt.group,
+      amount: Number(newGolUkt.amount), //pastika number
+    }
+     const saved = await addGolUkt(playload);
+      setGolUktList((prev) => [...prev, saved]);
+      setNewGolUkt({ group: "", amount: null });
+      fetchData();
+  } catch (error) {
+    console.log ("Gagal menambahkan Golongan UKT ==",error);
+  }
+}
+
+ const handleDelete = async(id : string) => {
+ if(!confirm("Apakah anda yakin ingin menghapus data ini?")) return;
+
+
+   try {
+    await deleteGolUkt(id);
+    setGolUktList((prev) => prev.filter((ukt) => ukt.id !== id));
+    alert("Data Golongan UKT berhasil dihapus!");
+    fetchData();
+   } catch (err: any) {
+     //cek error nya apakah karena  forekey constraint
+      const errorData = err.response?.data;
+      //cek apa pesan error apa yang di kirim dari backend contoh {"code": "P2003", "message": "Foreign key violation"}
+      const errorString = JSON.stringify(errorData || "").toLowerCase();
+      if (errorData?.code === "P2003") {
+        alert(
+          "Tidak dapat menghapus data karena masih terhubung dengan data lain",
+        );
+      } else {
+        alert("Terjadi kesalahan saat menghapus data" + err.message);
+      }
+
+      console.error("====Gagal delete golongan UKT ===", errorString);
+
+    }
+   }
+ 
+
   return (
     <section className="section">
       <div className="section-header">
@@ -34,13 +162,16 @@ const GolUKTPage = () => {
                 </button>
                 <div className="collapse" id="collapseEditGolonganUKT">
                   <div className="card card-body">
-                    <form>
+                    <form onSubmit={handleAddNewGolUkt}>
                       <div className="form-group">
                         <label>Golongan</label>
                         <input
                           type="text"
                           className="form-control"
                           placeholder="Nama Golongan"
+                          name="group"
+                          onChange={handleNewGolUktChange}
+                          required
                         />
                       </div>
                       <div className="form-group">
@@ -49,6 +180,10 @@ const GolUKTPage = () => {
                           type="number"
                           className="form-control"
                           placeholder="Jumlah"
+                          name="amount"
+                          value={newGolUkt.amount ?? ""}
+                          onChange={handleNewGolUktChange}
+                          required
                         />
                       </div>
                       <button type="submit" className="btn btn-primary">
@@ -69,79 +204,125 @@ const GolUKTPage = () => {
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <td>1</td>
-                        <td>Golongan I</td>
-                        <td>Rp 500.000</td>
-                        <td>2024-08-01</td>
-                        <td>
-                          <a href="#" className="btn btn-icon btn-primary">
-                            <i className="far fa-edit"></i>
-                          </a>
-                          <a href="#" className="btn btn-icon btn-danger">
-                            <i className="fa fa-trash"></i>
-                          </a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>2</td>
-                        <td>Golongan II</td>
-                        <td>Rp 1.000.000</td>
-                        <td>2024-08-02</td>
-                        <td>
-                          <a href="#" className="btn btn-icon btn-primary">
-                            <i className="far fa-edit"></i>
-                          </a>
-                          <a href="#" className="btn btn-icon btn-danger">
-                            <i className="fa fa-trash"></i>
-                          </a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>3</td>
-                        <td>Golongan III</td>
-                        <td>Rp 2.000.000</td>
-                        <td>2024-08-03</td>
-                        <td>
-                          <a href="#" className="btn btn-icon btn-primary">
-                            <i className="far fa-edit"></i>
-                          </a>
-                          <a href="#" className="btn btn-icon btn-danger">
-                            <i className="fa fa-trash"></i>
-                          </a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>4</td>
-                        <td>Golongan IV</td>
-                        <td>Rp 3.500.000</td>
-                        <td>2024-08-04</td>
-                        <td>
-                          <a href="#" className="btn btn-icon btn-primary">
-                            <i className="far fa-edit"></i>
-                          </a>
-                          <a href="#" className="btn btn-icon btn-danger">
-                            <i className="fa fa-trash"></i>
-                          </a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td>5</td>
-                        <td>Golongan V</td>
-                        <td>Rp 5.000.000</td>
-                        <td>2024-08-05</td>
-                        <td>
-                          <a href="#" className="btn btn-icon btn-primary">
-                            <i className="far fa-edit"></i>
-                          </a>
-                          <a href="#" className="btn btn-icon btn-danger">
-                            <i className="fa fa-trash"></i>
-                          </a>
-                        </td>
-                      </tr>
+                      {golUktList.map((ukt, index) => (
+                        <tr key={index}>
+                          <td>{index + 1}</td>
+                          <td>{ukt.group}</td>
+                          <td>{
+                            new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(ukt.amount)
+                            }</td>
+                          <td>{
+                            new Date(ukt.createdAt).toLocaleDateString('id-ID', {
+                              day: '2-digit',
+                              month: 'long',
+                              year: 'numeric',
+                            })
+                            }</td>
+                          <td>
+                            <a href="#" className="btn btn-icon btn-primary"
+                              onClick={() => openEditModal(ukt)}
+                            >
+                              <i className="far fa-edit"></i>
+                            </a>
+                            <a href="#" className="btn btn-icon btn-danger"
+                              onClick={() => handleDelete(ukt.id)}>
+                              <i className="fa fa-trash"></i>
+                            </a>
+                          </td>
+                        </tr>
+                      ))}
                     </tbody>
                   </table>
                 </div>
+
+                {/* Modal */}
+                {isEditModalOpen && (
+                  <div
+                    className="modal fade show"
+                    style={{
+                      display: "block",
+                      backgroundColor: "rgba(0,0,0,0.5)",
+                      position: "fixed",
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      zIndex: 1050,
+                    }}
+                  >
+                    <div className="modal-dialog modal-dialog-centered">
+                      <div className="modal-content">
+                        <form onSubmit={handleSave}>
+                          <div className="modal-header">
+                            <h5 className="modal-title">Edit UKT</h5>
+                            <button
+                              type="button"
+                              className="close"
+                              onClick={closeEditModal}
+                            >
+                              <span>&times;</span>
+                            </button>
+                          </div>
+                          <div className="modal-body">
+                            <div className="form-group">
+                              <label>Golongan UKT</label>
+                              <input
+                                type="text"
+                                className="form-control"
+                                name="group"
+                                value={selectedGolUkt?.group || ""}
+                                onChange={(e) => {
+                                  // Cek dulu apakah selectedFaculties ada isinya (tidak null)
+                                  if (selectedGolUkt) {
+                                    const newName = e.target.value;
+                                    setSelectedGolUkt({
+                                      ...selectedGolUkt, // Taruh spread di ATAS
+                                     group: newName,
+                                    });
+                                  }
+                                }}
+                                required
+                              />
+                            </div>
+                            <div className="form-group">
+                              <label>Jumlah</label>
+                              <input
+                                type="text"
+                                className="form-control"
+                                name="amount"
+                                value={selectedGolUkt?.amount || ""}
+                                onChange={(e) => {
+                                  if (selectedGolUkt) {
+                                    setSelectedGolUkt({
+                                      ...selectedGolUkt,
+                                      amount: Number(e.target.value),
+                                      
+                                    });
+                                  }
+                                }}
+                                required
+                              />
+                            </div>
+                          </div>
+                          <div className="modal-footer">
+                            <button
+                              type="button"
+                              className="btn btn-secondary"
+                              onClick={closeEditModal}
+                            >
+                              Batal
+                            </button>
+                            <button type="submit" className="btn btn-primary">
+                              Simpan
+                            </button>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </div>
             </div>
           </div>

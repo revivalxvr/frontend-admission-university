53b89fb8ccf0bdfd2dd977afdea29fabb01f47e0
revivalxvr <revivaliskandar@gmail.com>
2026-08-03 14:11:51 +0800

    dashboard

--- src/app/pages/admin/layout.tsx
+++ src/app/pages/admin/layout.tsx
@@ -1,29 +1,32 @@
-import React from 'react';
-import AdminNavbar from '../../components/AdminNavbar';
-import AdminSidebar from '../../components/AdminSidebar';
-import { ToastProvider } from '@/src/app/components/context/ToastContext'; 
+import React from "react";
+import AdminNavbar from "../../components/AdminNavbar";
+import AdminSidebar from "../../components/AdminSidebar";
+import { ToastProvider } from "@/src/app/components/context/ToastContext";
+import '@/src/app/globals.css';
+
 
 
 export const metadata = {
   title: "Dashboard Admin",
 };
 
-
 const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div id="app">
-      <div className="main-wrapper main-wrapper-1">
-        <div className="navbar-bg"></div>
-        <AdminNavbar />
-        <AdminSidebar />
-        <div className="main-content">
-          {/* 2. Bungkus children dengan ToastProvider */}
-          <ToastProvider>
-            {children}
-          </ToastProvider>
+    <html >
+      <body>
+        <div id="app">
+          <div className="main-wrapper main-wrapper-1">
+            <div className="navbar-bg"></div>
+            <AdminNavbar />
+            <AdminSidebar />
+            <div className="main-content">
+              {/* 2. Bungkus children dengan ToastProvider */}
+              <ToastProvider>{children}</ToastProvider>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </body>
+    </html>
   );
 };
 
